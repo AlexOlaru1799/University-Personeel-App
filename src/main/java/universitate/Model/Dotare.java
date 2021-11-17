d77ff81727e5183a3b14a3af89ef915ac4cfbb5d
cristian.olaru <cristian.olaru@mta.ro>
2021-11-17 19:44:34 +0200

    Creare clasa Dotare si clasa SalaDeClasa

---
+++ src/main/java/universitate/Model/Dotare.java
@@ -0,0 +1,17 @@
+package main.java.universitate.Model;
+
+public class Dotare {
+    String denumire;
+
+    public Dotare(String _denumire)
+    {
+        this.denumire = _denumire;
+    }
+
+
+    public void afiseazaDenumire()
+    {
+        System.out.println(this.denumire);
+    }
+
+}
