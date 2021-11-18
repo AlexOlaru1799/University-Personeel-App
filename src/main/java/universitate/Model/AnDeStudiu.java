0545d2b2145c1dca158450b14afc9c5d1a41cedf
stefan.oprea <stefan.oprea@mta.ro>
2021-11-18 18:02:16 +0200

    Clasele facute

--- src/main/java/universitate/Model/AnDeStudiu.java
+++ src/main/java/universitate/Model/AnDeStudiu.java
@@ -15,11 +15,4 @@ public class AnDeStudiu {
     public ArrayList<Specializare> getSpecializari() {
         return this.specializari;
     }
-
-    public String AfisareSpecializari(){
-        for(int i=0;i<this.specializari.size();i++)
-        {
-            return ("Specializarea: " + this.getSpecializari().get(i));
-        }
-    }
 }
