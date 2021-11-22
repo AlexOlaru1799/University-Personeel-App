96b618d459db5378f39c8afee92ecd3ee487a8fd
CazamirTeodor <cazamirteodor@gmail.com>
2021-11-22 20:09:44 +0200

    Repository restructured and uploaded initial Spring template

---
+++ webserver/src/main/java/mta/universitate/T1E1C114CApplication.java
@@ -0,0 +1,13 @@
+package mta.universitate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1E1C114CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1E1C114CApplication.class, args);
+	}
+
+}
---
+++ webserver/src/test/java/T1E1C114CApplicationTests.java
@@ -0,0 +1,13 @@
+package mta.universitate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1E1C114CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
