a41268b0c49102552fd814ff330219c3d225bb42
gabriela.chiforiuc <gabriela.chiforiuc@mta.ro>
2021-11-23 22:22:35 +0200

    Added query for Sali De Clasa and printed at /sali

--- webserver/src/main/java/mta/universitate/Model/SalaDeClasa.java
+++ webserver/src/main/java/mta/universitate/Model/SalaDeClasa.java
@@ -34,18 +34,24 @@ public class SalaDeClasa {
         ResultSet result = db1.executeQuery("select * from sali_de_clasa");
 
         if(result.next()==false)
-            return "trist:(";
-        //return result.getString(1);
+            return "Empty result";
+
         StringBuilder stringBuilder=new StringBuilder();
 
         ResultSetMetaData metadata = result.getMetaData();
         int columnCount = metadata.getColumnCount();
-        while (result.next()) {
-            for (int i = 1; i <= columnCount; i++) {
-                stringBuilder.append(result.getString(i)).append("\t");
-            }
-        }
 
+        stringBuilder.append("<style>table, th, td {border: 1px solid black;border-collapse: collapse; text-align:center} table.center{margin-left: auto;margin-right: auto;} table{font-family: arial, sans-serif;width: 70%;}</style>");
+        stringBuilder.append("<table class=\"center\"><tr><th>Id Sala</th><th>Denumire</th><th>Capacitate</th><th>Tipul Salii</th></tr>");
+
+        while (result.next()) {
+            stringBuilder.append("<tr>");
+            for (int i = 1; i <= columnCount; i++) {
+                stringBuilder.append("<td>").append(result.getString(i)).append("</td>");
+            }
+            stringBuilder.append("</tr>");
+        }
+        stringBuilder.append("</table>");
         return stringBuilder.toString();
     }
 
