b8d07fa59c2ad64fb94c6856c57254d3cbb451a5
gabriela.chiforiuc <gabriela.chiforiuc@mta.ro>
2021-11-23 12:52:45 +0200

    Adaugare functie getSali()

--- webserver/src/main/java/mta/universitate/Model/SalaDeClasa.java
+++ webserver/src/main/java/mta/universitate/Model/SalaDeClasa.java
@@ -1,20 +1,32 @@
 package mta.universitate.Model;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 enum TipSala{
     Amfiteatru,Laborator
 }
 
+@RestController
 public class SalaDeClasa {
     String denumire;
     int capacitate;
     Dotare dotari[ ];
     TipSala tipSala;
 
-    public SalaDeClasa(String _denumire,int _capacitate,TipSala _tip)
+
+
+//    public SalaDeClasa(String _denumire,int _capacitate,TipSala _tip)
+//    {
+//        this.denumire = _denumire;
+//        this.capacitate = _capacitate;
+//        this.tipSala = _tip;
+//    }
+
+    @RequestMapping("/sali")
+    public String getSali()
     {
-        this.denumire = _denumire;
-        this.capacitate = _capacitate;
-        this.tipSala = _tip;
+        return "<h1>Salile de clasa sunt aici!!!</h1>";
     }
 
     public void afiseazaDenumire()
