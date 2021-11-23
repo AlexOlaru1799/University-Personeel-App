35e5aa5b5de96f7d4aeb25a6a621e0ec9783701e
stefan.oprea <stefan.oprea@mta.ro>
2021-11-23 23:58:19 +0200

    Clasele GrupaDeStudiu si Specializare actualizate

--- webserver/src/main/java/mta/universitate/Model/GrupaDeStudiu.java
+++ webserver/src/main/java/mta/universitate/Model/GrupaDeStudiu.java
@@ -2,4 +2,14 @@ package mta.universitate.Model;
 
 public class GrupaDeStudiu {
 
+    private Profesor indrumator;
+    private Student studenti;
+
+    public Profesor getIndrumator() {
+        return indrumator;
+    }
+
+    public Student getStudenti() {
+        return studenti;
+    }
 }
