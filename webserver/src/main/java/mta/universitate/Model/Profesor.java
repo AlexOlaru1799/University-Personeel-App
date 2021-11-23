f59f7098dc6320911db486d19f4cac95928695c4
elena.ion <elena.ion@mta.ro>
2021-11-24 01:41:08 +0200

    Added getMySchedule method into class Profesor

--- webserver/src/main/java/mta/universitate/Model/Profesor.java
+++ webserver/src/main/java/mta/universitate/Model/Profesor.java
@@ -1,5 +1,13 @@
 package mta.universitate.Model;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+@RestController
 public class Profesor extends Angajat{
 
     public Profesor()
@@ -17,5 +25,60 @@ public class Profesor extends Angajat{
         System.out.print("Profesor adauga nota" +
                 nota + "studentului " +S.getNume());
     }
+
+    @RequestMapping("/orar")
+    public String getMySchedule() throws SQLException
+    {
+        Database db1;
+        db1 = Database.getInstance();
+        ResultSet result = db1.executeQuery(
+                "SELECT [dbo].[orar].[Ora], [dbo].[grupe_studiu].[ID_Grupa], [dbo].[materii].[ID_Materie], [dbo].[angajati].[Nume],[dbo].[angajati].[Prenume]" +
+                        "FROM [dbo].[angajati]" +
+                        "INNER JOIN [dbo].[functii]" +
+                        "ON [dbo].[functii].[ID_Functie]=[dbo].[angajati].[ID_Angajat]" +
+                        "INNER JOIN [dbo].[ore]" +
+                        "ON [dbo].[ore].[FK_Titular]=[dbo].[angajati].[ID_Angajat]" +
+                        "INNER JOIN [dbo].[orar]" +
+                        "ON [dbo].[orar].[FK_Ore]=[dbo].[ore].[ID_Ora]" +
+                        "INNER JOIN [dbo].[grupe_studiu]" +
+                        "ON [dbo].[grupe_studiu].[ID_Grupa]=[dbo].[orar].[FK_Grupa]" +
+                        "INNER JOIN [dbo].[materii]" +
+                        "ON [dbo].[materii].[FK_Profesor]=[dbo].[angajati].[ID_Angajat]" +
+                        "WHERE [dbo].[functii].[Denumire]='Profesor'" +
+                        "ORDER BY [dbo].[angajati].[Nume] ASC");
+
+
+        if(result == null)
+        {
+            return "Eroare :(";
+        }
+
+        ResultSetMetaData metadata = result.getMetaData();
+        int columnCount = metadata.getColumnCount();
+
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("<style>table, th, td {border: 1px solid black;border-collapse: collapse; text-align:center} table.center{margin-left: auto;margin-right: auto;} table{font-family: arial, sans-serif;width: 70%;}</style>");
+        stringBuilder.append("<table class=\"center\">" +
+                "<tr>" +
+                "<th>Ora</th>" +
+                "<th>ID Grupa</th>" +
+                "<th>ID Materie</th>" +
+                "<th>Nume profesor</th>" +
+                "<th>Prenume profesor</th>" +
+                "</tr>");
+
+        while (result.next()) {
+            stringBuilder.append("<tr>");
+            for (int i = 1; i <= columnCount; i++) {
+                stringBuilder.append("<td>").append(result.getString(i)).append("</td>");
+            }
+            stringBuilder.append("</tr>");
+        }
+        stringBuilder.append("</table>");
+
+
+        return stringBuilder.toString();
+    }
+
 
 }
--- webserver/src/main/java/mta/universitate/Model/Secretar.java
+++ webserver/src/main/java/mta/universitate/Model/Secretar.java
@@ -1,5 +1,20 @@
 package mta.universitate.Model;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+
+/**
+ * Clasa Secretar
+ *
+ * Clasa utilizata pentru a implementa utilitatile secretarului
+ *
+ */
+@RestController
 public class Secretar extends Angajat{
 
     public Secretar()
@@ -7,10 +22,57 @@ public class Secretar extends Angajat{
         super();
     }
     public Secretar(String _nume,String _prenume,
-                   String _dataNasterii)
+                    String _dataNasterii)
     {
         super(_nume,_prenume,_dataNasterii);
     }
+
+    @RequestMapping("/studenti")
+    public String getStudenti() throws SQLException {
+        Database db1;
+        db1 = Database.getInstance();
+        ResultSet result = db1.executeQuery(
+                "SELECT [dbo].[studenti].Nume,[dbo].[studenti].Prenume,[dbo].[specializari].Denumire,[dbo].[grupe_studiu].denumire_grupa " +
+                        "FROM [dbo].[studenti] " +
+                        "INNER JOIN [dbo].[specializari] " +
+                        "ON [dbo].[studenti].FK_Specializare = [dbo].[specializari].ID_Specializare " +
+                        "INNER JOIN [dbo].[grupe_studiu] " +
+                        "ON [dbo].[studenti].FK_Grupa = [dbo].[grupe_studiu].ID_Grupa " +
+                        "ORDER BY [dbo].[grupe_studiu].denumire_grupa ASC");
+
+
+        if(result == null)
+        {
+            return "Eroare :(";
+        }
+
+        ResultSetMetaData metadata = result.getMetaData();
+        int columnCount = metadata.getColumnCount();
+
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("<style>table, th, td {border: 1px solid black;border-collapse: collapse; text-align:center} table.center{margin-left: auto;margin-right: auto;} table{font-family: arial, sans-serif;width: 70%;}</style>");
+        stringBuilder.append("<table class=\"center\">" +
+                "<tr>" +
+                "<th>Nume Student</th>" +
+                "<th>Prenume Student</th>" +
+                "<th>Specializare</th>" +
+                "<th>Grupa Student</th>" +
+                "</tr>");
+
+        while (result.next()) {
+            stringBuilder.append("<tr>");
+            for (int i = 1; i <= columnCount; i++) {
+                stringBuilder.append("<td>").append(result.getString(i)).append("</td>");
+            }
+            stringBuilder.append("</tr>");
+        }
+        stringBuilder.append("</table>");
+
+
+        return stringBuilder.toString();
+
+    }
+
 
     public void afiseazaStatistici(Student S)
     {
@@ -19,18 +81,15 @@ public class Secretar extends Angajat{
     }
 
 
-
-    //TO DO statistici pentru grupa
-//    public void afiseazaStatistici(Grupa G)
-//    {
-//        System.out.print("Afisare statistici student "+
-//                G.getNume());
-//    }
+    public void afiseazaStatistici(GrupaDeStudiu G)
+    {
+        System.out.print("Afisare gurpa de studiu");
+    }
 
     //TO DO statistici pentru an de studiu
-//    public void afiseazaStatistici(AnDeStudiu A)
-//    {
-//        System.out.print("Afisare statistici student "+
-//                G.getNume());
-//    }
+    public void afiseazaStatistici(AnDeStudiu A)
+    {
+        System.out.print("An "+
+                A.getAn());
+    }
 }
