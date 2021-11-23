054d9ddf3e3f7cfad8c36a425864bc40f874f235
Marian Razvan <marianrazvan84@yahoo.com>
2021-11-23 22:36:17 +0200

    Adaugare functie vizualizare studenti - Secretar

--- webserver/src/main/java/mta/universitate/Model/Secretar.java
+++ webserver/src/main/java/mta/universitate/Model/Secretar.java
@@ -3,6 +3,10 @@ package mta.universitate.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 
 /**
  * Clasa Secretar
@@ -24,26 +28,49 @@ public class Secretar extends Angajat{
     }
 
     @RequestMapping("/studenti")
-    public String afiseazaStudenti()
-    {
+    public String getStudenti() throws SQLException {
+        Database db1;
+        db1 = Database.getInstance();
+        ResultSet result = db1.executeQuery(
+                "SELECT [dbo].[studenti].Nume,[dbo].[studenti].Prenume,[dbo].[specializari].Denumire,[dbo].[grupe_studiu].denumire_grupa " +
+                        "FROM [dbo].[studenti] " +
+                        "INNER JOIN [dbo].[specializari] " +
+                        "ON [dbo].[studenti].FK_Specializare = [dbo].[specializari].ID_Specializare " +
+                        "INNER JOIN [dbo].[grupe_studiu] " +
+                        "ON [dbo].[studenti].FK_Grupa = [dbo].[grupe_studiu].ID_Grupa " +
+                        "ORDER BY [dbo].[grupe_studiu].denumire_grupa ASC");
 
-        return "<table border >" +
-                "  <tr>" +
-                "    <th>Grupa</th>" +
-                "    <th>Nume</th>" +
-                "    <th>Prenume</th>" +
-                "  </tr>" +
-                "  <tr>" +
-                "    <td>C114C</td>" +
-                "    <td>Marian</td>" +
-                "    <td>Razvan</td>" +
-                "  </tr>" +
-                "  <tr>" +
-                "    <td>C114C</td>" +
-                "    <td>Chiforiuc</td>" +
-                "    <td>Gabriela</td>" +
-                "  </tr>" +
-                "</table>";
+
+        if(result == null)
+        {
+            return "Eroare :(";
+        }
+
+        ResultSetMetaData metadata = result.getMetaData();
+        int columnCount = metadata.getColumnCount();
+
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("<style>table, th, td {border: 1px solid black;border-collapse: collapse; text-align:center} table.center{margin-left: auto;margin-right: auto;} table{font-family: arial, sans-serif;width: 70%;}</style>");
+        stringBuilder.append("<table class=\"center\">" +
+                "<tr>" +
+                "<th>Nume Student</th>" +
+                "<th>Prenume Student</th>" +
+                "<th>Specializare</th>" +
+                "<th>Grupa Student</th>" +
+                "</tr>");
+
+        while (result.next()) {
+            stringBuilder.append("<tr>");
+            for (int i = 1; i <= columnCount; i++) {
+                stringBuilder.append("<td>").append(result.getString(i)).append("</td>");
+            }
+            stringBuilder.append("</tr>");
+        }
+        stringBuilder.append("</table>");
+
+
+        return stringBuilder.toString();
+
     }
 
 
