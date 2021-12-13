2d7b10a2e41f66bd80b06e9d7a9d5db6cd51bd8d
CazamirTeodor <cazamirteodor@gmail.com>
2021-12-13 17:45:29 +0200

    Added routes

---
+++ webserver/src/main/java/mta/universitate/Routes/RouteStudent.java
@@ -0,0 +1,31 @@
+package mta.universitate.Routes;
+
+import mta.universitate.Model.Database;
+import mta.universitate.Model.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLException;
+
+public class RouteStudent {
+    Database db = Database.getInstance();
+
+    @RequestMapping(value = "/student/my-profile", produces = "application/json")
+    @ResponseBody
+    public String profile(){
+
+        try
+        {
+            // TODO
+            // Parse cookie so I can return the Student object
+            // Student S = CookieManager.parseCookie(cookie);
+            // return S.toJSON();
+            return "{'status' : 'SUCCESS'}";
+        }
+        catch (SQLException e){}
+
+        return "{'status' : 'FAILED'}";
+    }
+
+}
