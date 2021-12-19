f518744af2d9bbe59016b88e70acd4446db364d8
cristian.olaru <cristian.olaru@mta.ro>
2021-12-19 18:27:08 +0200

    Added create teacher function with backend

--- Vaadin-frontend/src/main/java/com/example/application/views/Secretary/SignOut.java
+++ Vaadin-frontend/src/main/java/com/example/application/views/Secretary/SignOut.java
@@ -2,11 +2,18 @@
 
 package com.example.application.views.Secretary;
 
+        import com.example.application.views.Utils.ApiRequest;
+        import com.example.application.views.Utils.OwnCookieManager;
         import com.vaadin.flow.component.button.Button;
         import com.vaadin.flow.component.html.H1;
         import com.vaadin.flow.component.orderedlayout.VerticalLayout;
         import com.vaadin.flow.router.PageTitle;
         import com.vaadin.flow.router.Route;
+
+        import java.net.CookieManager;
+        import java.net.HttpCookie;
+        import java.util.HashMap;
+        import java.util.List;
 
 @PageTitle("Sign Out")
 @Route(value = "signOut", layout = MainLayout.class)
@@ -21,6 +28,18 @@ public class SignOut extends VerticalLayout {
         leave = new Button("Leave");
         add(text,leave);
         leave.addClickListener(e -> {
+            // Send the request and get the response
+            HashMap<String, Object> response;
+            CookieManager cookieManager = new CookieManager();
+
+            // Get the cookie and store it in the CookieManager
+            //cookieManager.getCookieStore().add(null, req.getCookie());
+
+
+            ApiRequest  req = new ApiRequest("http://localhost:8080/logout");
+            // Add cookie to the request
+            req.addCookie(OwnCookieManager.getInstance().getCookie());
+            response = req.send();
             getUI().ifPresent(ui ->ui.navigate(""));
         });
 
