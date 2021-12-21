014d22c13f9e613d4d24c46db30709f16562e162
stefan.oprea <stefan.oprea@mta.ro>
2021-12-21 16:19:33 +0200

    Add functions UI Prof

--- Vaadin-frontend/src/main/java/com/example/application/views/Professor/SubjectFailed.java
+++ Vaadin-frontend/src/main/java/com/example/application/views/Professor/SubjectFailed.java
@@ -6,10 +6,15 @@ import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @PageTitle("Subject Failed")
 @Route(value = "subjectFailed", layout = ProfessorLayout.class)
@@ -30,17 +35,72 @@ public class SubjectFailed extends VerticalLayout{
         layout.setPadding(true);
         layout.add(showSubjects);
 
+        VerticalLayout infoLayout = createLayout("Student who failed at one exam: ");
+        TextArea textArea = new TextArea();
+        textArea.setWidth("500px");
+        textArea.isReadOnly();
+        textArea.setHeight("500px");
+        infoLayout.add(textArea);
 
         showSubjects.addClickListener(e -> {
 
+            ApiRequest req = new ApiRequest("http://localhost:8080/professor/failedOneSubject");
+            req.addCookie(OwnCookieManager.getInstance().getCookie());
 
+            HashMap<String, Object> response = req.send();
 
-            //Database DB = Database.getInstance();
+            System.out.println(response);
 
-            //ResultSet res2 = DB.getStudentInfo(ID);
+            ArrayList<Object> objResp = new ArrayList<Object>(response.values());
 
-            //ResultSet res3 = DB.getStudentGrades(ID);
+            ArrayList<Object> objClasses = (ArrayList<Object>)objResp.get(0);
 
+            System.out.println(objClasses);
+
+            ArrayList<String> name = new ArrayList<String>(objClasses.size());
+            ArrayList<String> surname = new ArrayList<String>(objClasses.size());
+            ArrayList<String> courseName = new ArrayList<String>(objClasses.size());
+            ArrayList<String> studyGroup = new ArrayList<String>(objClasses.size());
+
+            for (int i = 0; i < objClasses.size(); i++) {
+                String[] set = null;
+                String c = objClasses.get(i).toString();
+                set = c.split("=");
+                String nume = set[9].split(",")[0];
+                String prenume = set[10].split(",")[0];
+                String materie = set[3].split(",")[0];
+                String grupa = set[12].split(",")[0];
+                set = set[1].split(",");
+                name.add(nume);
+                surname.add(prenume);
+                courseName.add(materie);
+                studyGroup.add(grupa);
+            }
+
+            String failedStudents = "";
+            for(int i = 0;i < name.size(); i++)
+            {
+                failedStudents += name.get(i);
+                failedStudents += "\t";
+                failedStudents += surname.get(i);
+                failedStudents += "\t";
+                failedStudents += studyGroup.get(i);
+                failedStudents += "\t";
+                failedStudents += courseName.get(i);
+                failedStudents += "\n";
+            }
+
+            if(response.get("status").equals("SUCCESS"))
+            {
+                if(response.get("result") != null ) {
+                    textArea.setValue(failedStudents);
+                }
+                Notification.show("Print succes!");
+            }
+            else
+            {
+                Notification.show("Failed :(");
+            }
         });
     }
 
