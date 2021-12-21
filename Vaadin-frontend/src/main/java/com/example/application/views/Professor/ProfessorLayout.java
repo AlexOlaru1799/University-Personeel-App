89896efc575bc6880f2b7c82a342046ed1f9dc43
cristian.olaru <cristian.olaru@mta.ro>
2021-12-21 03:52:03 +0200

    Added full functionality for the viewTeacher function in Secretary UI

--- Vaadin-frontend/src/main/java/com/example/application/views/Professor/ProfessorLayout.java
+++ Vaadin-frontend/src/main/java/com/example/application/views/Professor/ProfessorLayout.java
@@ -113,16 +113,16 @@ public class ProfessorLayout extends AppLayout {
 
                 new MenuItemInfo("Subject Statistics", "la la-search", SubjectStatistics.class), //
 
-<<<<<<< HEAD
+
 
                 new MenuItemInfo("Add Grade", "la  la-smile-o", AddGrade.class), //
 
                 new MenuItemInfo("Sign Out", "la la-times-circle-o", SignOut.class), //
-=======
+
                 new MenuItemInfo("Add Grade", "la  la-smile-o", AddGrade.class), //
 
                 new MenuItemInfo("Student Grades", "la  la-smile-o", GradesStudent.class), //
->>>>>>> master
+
 
                 new MenuItemInfo("Sign Out", "la la-times-circle-o", SignOutProfessor.class), //
 
--- Vaadin-frontend/src/main/java/com/example/application/views/Secretary/ViewTeacher.java
+++ Vaadin-frontend/src/main/java/com/example/application/views/Secretary/ViewTeacher.java
@@ -1,212 +1,148 @@
 
 package com.example.application.views.Secretary;
-     // To do modify for the current project
 
 
+
+import com.example.application.views.Utils.ApiRequest;
+import com.example.application.views.Utils.OwnCookieManager;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.H5;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-
-
+import java.util.Collection;
+import java.util.HashMap;
 
 @PageTitle("View Teacher")
 @Route(value = "viewTeacher", layout = MainLayout.class)
 public class ViewTeacher extends VerticalLayout {
 
-    private TextField name;
-    private Button searchButton;
-
     public ViewTeacher() {
-        name = new TextField("Teacher ID");
-        searchButton = new Button("Search");
+        VerticalLayout mainLayout = createLayout("Insert the name/surname of the teacher");
 
-        setSpacing(true);
-        setPadding(true);
-
-        H5 nume = new H5("");
-        H5 prenume = new H5("");
-        H5 salariu = new H5("");
-        H5 id = new H5("");
-
-        H5 username = new H5("");
+        TextField name = new TextField("Teacher name");
+        name.setWidth("500px");
+        TextField surname = new TextField("Teacher surname");
+        surname.setWidth("500px");
 
 
+        Button addStudent = new Button("View Teacher Information");
+        addStudent.setWidth("500px");
 
-        add(new H3("Search the teacher using his ID"));
+        mainLayout.add(name,surname,addStudent);
+        add(mainLayout);
 
-        VerticalLayout layout = createLayout("Enter the ID here");
-        layout.setPadding(true);
-        layout.add(name,searchButton);
-
-
-        layout = createLayout("Teacher info : ");
-        layout.setPadding(true);
-        layout.setHeight("300px");
-        layout.add(id,nume,prenume,salariu,username);
-        layout.getStyle().set("overflow", "scroll");// enable scrolling when content doesn't fit
+        VerticalLayout infoLayout = createLayout("");
 
 
-        layout = createLayout("Teacher classes : ");
-        layout.setPadding(true);
-        layout.setHeight("500px");
-        layout.getStyle().set("overflow", "scroll");// enable scrolling when content doesn't fit
-
-        List<H5> materii = new ArrayList<H5>(50);
-        List<H5> tip = new ArrayList<H5>(50);
-        List<H5> ora = new ArrayList<H5>(50);
-        List<H5> grupa = new ArrayList<H5>(50);
-        List<H5> sala = new ArrayList<H5>(50);
-
-        H3 titluM = new H3("Materie");
-        H3 titluT = new H3("Tip");
-        H3 tittluO = new H3("Ora");
-        H3 titluG = new H3("Grupa");
-        H3 titluS = new H3("Sala");
+        H3 infoStudent = new H3("");
+        infoStudent.getStyle().set("color", "blue");
+        infoStudent.getStyle().set("fontWeight", "bold");
+        H5 numeInfo = new H5("");
+        H5 prenumeInfo = new H5("");
+        H5 incomeInfo = new H5("");
+        H5 teacherUsername = new H5("");
+        H5 teacherPosition = new H5("");
+        H5 teacherRole = new H5("");
 
 
+        infoLayout.add(infoStudent,numeInfo,prenumeInfo,incomeInfo,teacherPosition,teacherRole,teacherUsername);
 
-        HorizontalLayout titles = new HorizontalLayout(titluM,titluT,tittluO,titluG,titluS);
-        layout.add(titles);
+        //,numeInfo,prenumeInfo,studentUsername,studentPassword,incomeInfo,studyGroup,nameGrupa,anGrupa,mentorInfo,mentorName,mentorSurname,mentorSalary,mentorPosition,mentorUsername,mentorPassword);
 
-        List<HorizontalLayout> laySS = new ArrayList<HorizontalLayout>(50);
+        //infoLayout.add(facultyInfo,major,faculty);
 
-        for(int i =0;i<50;i++)
-        {
+        add(infoLayout);
 
-            materii.add(new H5(""));
-            tip.add(new H5(""));
-            ora.add(new H5(""));
-            grupa.add(new H5(""));
-            sala.add(new H5(""));
+        addStudent.addClickListener(e -> {
+            String nameS = name.getValue();
+            String surnameS = surname.getValue();
+            ApiRequest req = new ApiRequest("http://localhost:8080/secretary/view-professor");
 
-            laySS.add(new HorizontalLayout(materii.get(i),tip.get(i),ora.get(i),grupa.get(i),sala.get(i)));
+            //ApiRequest req = new ApiRequest("http://localhost:8080/secretary/view-students");
+            req.addCookie(OwnCookieManager.getInstance().getCookie());
+            req.addParameter("name",nameS);
+            req.addParameter("surname",surnameS);
+            HashMap<String, Object> response = req.send();
+            ArrayList<Object> objects = new ArrayList<Object>(response.values());
 
-            //laySS.add(materii.get(i),tip.get(i),ora.get(i),grupa.get(i),sala.get(i));
 
-            layout.add(laySS.get(i));
-        }
+            Object studs_json = objects.get(0);
 
 
 
 
-        var ref = new Object() {
-            int counter = 0;
-        };
 
 
 
-        searchButton.addClickListener(e -> {
-
-            nume.setText("");
-            prenume.setText("");
-            id.setText("");
-            salariu.setText("");
-
-            username.setText("");
-
-            
 
 
-//            for(int i =0;i<50;i++) {
-//
-//                materii.get(i).setText("");
-//                tip.get(i).setText("");
-//                ora.get(i).setText("");
-//                grupa.get(i).setText("");
-//                sala.get(i).setText("");
-//
-//            }
-//
-//            ref.counter=0;
-//
-//            String ID = name.getValue();
-//
-//            Database DB = Database.getInstance();
-//
-//            ResultSet res = DB.getProfessorInfo(ID);
-//            ResultSet res2 = DB.getProfessorInfo(ID);
-//
-//            ResultSet res3 = DB.getProfessorClasses(ID);
-//
-//
-//
-//            try {
-//                if(!res2.next())
-//                {
-//                    Notification.show("Teacher with ID:" + ID + " does not exist in the database!");
-//                    nume.setText("Teacher with ID:" + ID + " does not exist in the database!");
-//                }
-//                else
-//                {
-//                    try {
-//                        while (res.next()) {
-//
-//
-//                            id.setText("ID : "+ res.getString("ID") );
-//                            nume.setText("Nume : "+res.getString("NUME") );
-//                            prenume.setText("Prenume : "+res.getString("PRENUME") );
-//                            salariu.setText("Salariu : "+res.getString("SALARIU") );
-//                            username.setText("Username : "+res.getString("USERNAME") );
-//                        }
-//
-//                    } catch (SQLException ex) {
-//                        ex.printStackTrace();
-//                    }
-//
-//                    try {
-//
-//                        while (res3.next()) {
-//
-//
-//                            materii.get(ref.counter).setText(res3.getString("MATERIE"));
-//                            tip.get(ref.counter).setText(res3.getString("TIP"));
-//                            ora.get(ref.counter).setText(res3.getString("ORA"));
-//                            grupa.get(ref.counter).setText(res3.getString("GRUPA"));
-//                            sala.get(ref.counter).setText(res3.getString("DENUMIRE"));
-//
-//
-//
-//                            ref.counter++;
-//
-//
-//                        }
-//
-//
-//                    } catch (SQLException ex) {
-//                        ex.printStackTrace();
-//                    }
-//                }
-//            } catch (SQLException ex) {
-//                ex.printStackTrace();
-//            }
-//
-//
+            if(response.get("status").equals("SUCCESS")) {
+
+                Notification.show("Student with name: " + nameS + " " + surnameS + " has this info");
+
+                try {
+
+                    JSONObject obj = new JSONObject(studs_json.toString());
+
+
+                    String incomeInfoS = obj.getString("salary");
+                    String teacherPositionS = obj.getJSONObject("position").getString("description");
+                    String teacherUsernameS = obj.getJSONObject("user").getString("username");
+                    String teacherRoleS = obj.getJSONObject("user").getJSONObject("role").getString("description");
+
+
+                    infoStudent.setText("Teacher Information : ");
+                    numeInfo.setText("#Name: " + nameS);
+                    prenumeInfo.setText("#Surname: " + surnameS);
+                    teacherPosition.setText("#Position : " + teacherPositionS);
+                    teacherRole.setText("#Role : " + teacherRoleS);
+                    teacherUsername.setText("#Teacher Username: " + teacherUsernameS);
+
+                    incomeInfo.setText("#Monthly Income : " + incomeInfoS + " lei");
+
+
+
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+
+
+
+
+
+
+                // numeInfo.setText( listOfValues.get(0).toString());
+            }
+            else{
+                Notification.show("There is no Student with this name!");
+            }
+
+            name.clear();
+            surname.clear();
+
+
+
+
         });
-
-
-
-        //add(name, sayHello);
-
-
-
-
     }
 
     private VerticalLayout createLayout(String caption) {
         VerticalLayout hl = new VerticalLayout();
         hl.setWidth("600px");
         hl.getStyle().set("background-color", "#e8ebef");
-        add(new H4(caption));
+        //add(new H1(caption));
+        hl.add(new H3(caption));
+
         add(hl);
         add(new Html("<span>&nbsp;</span>")); // spacer
         return hl;
